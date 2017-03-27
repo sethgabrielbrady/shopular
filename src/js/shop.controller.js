81b9f38ef63a6e1dbb8f1dde26aaba26e24a4aa7
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-27 12:31:51 -0400

    File structure set up for master

---
+++ src/js/shop.controller.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+angular.module('shopular')
+  .controller('ShopController', ShopController);
+
+  function ShopController(){
+
+
+
+  }//belongs to ShopController
+
+}());
