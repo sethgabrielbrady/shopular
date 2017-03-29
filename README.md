23e3ad4018c06dbecca74bf855721fc78151416a
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-28 20:53:32 -0400

    minor fixes and organization for better readability

--- README.md
+++ README.md
@@ -1,54 +1,28 @@
 # Shopular
 ## Seth Brady
-### Front End 03/28/17
-### Set Up
 
+
+### Set Up
 1. clone from github
 2. open in server
 
-add label to input boxes
-sort table (tbody)
-fix data styling
 
+### Front End 03/28/17
 
+Going from the last project, we are building on what we know in ``angular`` to practice ``two-way
+data binding.``
 
-## Learning Objective
+## TODO
 
-Angular has two-way data binding - which means it will automatically marshal data from the UI to the controller and back. This works even with multiple uses of the data in the UI! Let's get some practice with this concept. By the end of this assignment you should understand how to bind form inputs to data on a controller's scope and then see any changes in the view. We'll also see how Angular allows us to validate form data and use directives to initiate actions.
-
-Your Mission
-
-Start with our Item Shop from the previous assignment. We want to allow the owner to add new items to the inventory and sort items in the table.
-
-1. Create a form to add a new item
-The owner must provide the name, price, quantity, color, and discount
-2. Use Angular directives to bind the data to the controller (use a newItem object on the scope)
-3. Validate the data using angular, highlight any invalid fields in red. On form submission, add the item to the data (and hence UI) if all fields are valid
-4. Allow sorting of the items in the display: default to sorting by price (lowest at the top)
-  allow a user to click on a table header and sort by: name, quantity, or price
-
-## Adventure Mode
-
-1. Clicking on a header multiple times alternates between ascending and descending sort
-
-## EPIC Mode
-
+0. add text fields to incorrect inputs
 1. Allow the owner to change the quantity of an item up or down in the table.
 2. Allow an owner to remove an item from the inventory, but not if there is any quantity remaining.
 
 
 
 ### Front End 03/27/17
-
-
-### Set Up
-
-1. clone from github
-2. open in server
-
-
 This is practice with the basics of ``Angular`` building a ``web application``
-for a shop's inventory. At the end of this project I should have a basic
+for a shop's inventory. At the end of this project we should have a basic
 grasp of putting a couple of the pieces of ``Angular`` together to see data in
 our browser.
 
--- src/js/shop.controller.js
+++ src/js/shop.controller.js
@@ -11,112 +11,28 @@
      */
     function ShopController() {
 
-      let vm = this;
-      vm.sortType = 'price';
-      vm.sortReverse = false;
+        let clickCount = 0;
+        let vm = this;
+        vm.taxes = taxes;
+        vm.sortType = 'price';
+        vm.sortReverse = false;
+        vm.newItem = {};
 
-      vm.newItem = {};
-
-        vm.inventory = [{
-                "id": 2957,
-                "name": "widget",
-                "price": 32,
-                "quantity": 203,
-                "color": "red",
-                "discount": 31
-            },
-            {
-                "id": 89274,
-                "name": "golf club",
-                "price": 98,
-                "quantity": 10,
-                "color": "black",
-                "discount": 0
-            },
-            {
-                "id": 64,
-                "name": "iPhone",
-                "price": 499,
-                "quantity": 2,
-                "color": "white",
-                "discount": 0
-            },
-            {
-                "id": 87363,
-                "name": "bonzai tree",
-                "price": 76,
-                "quantity": 2,
-                "color": "green",
-                "discount": 0
-            },
-            {
-                "id": 1736,
-                "name": "towel",
-                "price": 55,
-                "quantity": 30,
-                "color": "brown",
-                "discount": 10
-            },
-            {
-                "id": 4836,
-                "name": "dog bed",
-                "price": 99,
-                "quantity": 10,
-                "color": "beige",
-                "discount": 50
-            },
-            {
-                "id": 829,
-                "name": "waste basket",
-                "price": 15,
-                "quantity": 40,
-                "color": "silver",
-                "discount": 0
-            },
-            {
-                "id": 46,
-                "name": "guitar",
-                "price": 899,
-                "quantity": 0,
-                "color": "blue",
-                "discount": 150
-            },
-            {
-                "id": 17456,
-                "name": "matcha tea",
-                "price": 42,
-                "quantity": 4,
-                "color": "green",
-                "discount": 11
-            },
-            {
-                "id": 3292,
-                "name": "enlightenment",
-                "price": 99999,
-                "quantity": 1,
-                "color": "red",
-                "discount": 0
-            },
-            {
-                "id": 533,
-                "name": "eggs",
-                "price": 5,
-                "quantity": 12,
-                "color": "brown",
-                "discount": 1
-            },
-            {
-                "id": 683,
-                "name": "pillow",
-                "price": 27,
-                "quantity": 10,
-                "color": "black",
-                "discount": 12
-            }
+        vm.inventory = [
+        { "id": 2957, "name": "widget", "price": 32, "quantity": 203, "color": "red", "discount": 31 },
+        { "id": 89274, "name": "golf club", "price": 98, "quantity": 10, "color": "black", "discount": 0 },
+        { "id": 64, "name": "iPhone", "price": 499, "quantity": 2, "color": "white", "discount": 0 },
+        { "id": 87363, "name": "bonzai tree", "price": 76, "quantity": 2, "color": "green", "discount": 0 },
+        { "id": 1736, "name": "towel", "price": 55, "quantity": 30, "color": "brown", "discount": 10 },
+        { "id": 4836, "name": "dog bed", "price": 99, "quantity": 10, "color": "beige", "discount": 50 },
+        { "id": 829, "name": "waste basket", "price": 15, "quantity": 40, "color": "silver", "discount": 0 },
+        { "id": 46, "name": "guitar", "price": 899, "quantity": 0, "color": "blue", "discount": 150 },
+        { "id": 17456, "name": "matcha tea", "price": 42, "quantity": 4, "color": "green", "discount": 11 },
+        { "id": 3292, "name": "enlightenment", "price": 99999, "quantity": 1, "color": "red", "discount": 0 },
+        { "id": 533, "name": "eggs", "price": 5, "quantity": 12, "color": "brown", "discount": 1 },
+        { "id": 683, "name": "pillow", "price": 27, "quantity": 10, "color": "black", "discount": 12 }
         ];
 
-        
-        vm.taxes = taxes;
 
         /**
          * takes in the inventory object, subtracts the discount property value from the
@@ -136,8 +52,6 @@
         vm.changeCountry = function changeCountry() {
             document.querySelector('.change').innerHTML = 'COLOUR';
         };
-
-        let clickCount = 0;
 
         /**
          *Will change mulitply the inventory price value by 1.5 and change
@@ -160,41 +74,31 @@
         }
 
         /**
-         * Add a new object to the inventory array
+         * Add a new object to the inventory array. Will convert a string to a
+         * number for price, discount, and quantity inputs
          * @param {String|Number} item
          */
         vm.addItem = function addItem(item) {
-          console.log(item);
+            item.price = Number(item.price);
+            item.discount = Number(item.discount);
+            item.quantity = Number(item.quantity);
 
-          item.price = Number(item.price);
-          item.discount = Number(item.discount);
-          item.quantity = Number(item.quantity);
+            if (typeof(item) !== 'object' || typeof(item.name) !== 'string' ||
+                typeof(item.price) !== 'number' || typeof(item.discount) !== 'number' ||
+                typeof(item.quantity) !== 'number' || typeof(item.quantity) === NaN ||
+                typeof(item.price) === NaN || typeof(item.discount) === NaN) {
+                return;
+            }
 
-
-          if (typeof(item)!== 'object' || typeof(item.name)!== 'string'
-          || typeof(item.price) !== 'number' || typeof(item.discount)!== 'number'
-          || typeof(item.quantity) !== 'number' || typeof(item.quantity) === NaN
-          || typeof(item.price) === NaN || typeof(item.discount) === NaN){
-            return;
-          }
-
-          vm.inventory.push({
-            name: item.name,
-            price: item.price,
-            quantity: item.quantity,
-            discount: item.discount,
-            color: item.color
-          });
+            vm.inventory.push({
+                name: item.name,
+                price: item.price,
+                quantity: item.quantity,
+                discount: item.discount,
+                color: item.color
+            });
         }
 
 
-
-          // return typeCol;
-
-
     } //belongs to ShopController
-
-
-
-
 }());
